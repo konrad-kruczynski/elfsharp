55c78081a54cb5a8e8f8a34bbd05991afa055cf3
Konrad Kruczyński <konrad.kruczynski@gmail.com>
2011-09-25 17:41:20 +0200

    Couple bug fixes.

--- ELFSharp/SymbolEntry.cs
+++ ELFSharp/SymbolEntry.cs
@@ -2,13 +2,14 @@
 
 namespace ELFSharp
 {
-    public class SymbolEntry
+    public abstract class SymbolEntry
     {
-        public string Name { get; private set; }
-        public UInt32 Value { get; private set; }
-        public UInt32 Size { get; private set; }
+        public string Name { get; private set; }        
         public SymbolBinding Binding { get; private set; }
         public SymbolType Type { get; private set; }
+		
+		protected UInt64 LongValue { get; private set; }
+        protected UInt64 LongSize { get; private set; }
 
         public bool IsSpecialSection
         {
@@ -25,11 +26,11 @@ namespace ELFSharp
             get { return IsSpecialSection ? (ushort)0 : sectionIdx; }
         }
 
-        public SymbolEntry(string name, uint value, uint size, SymbolBinding binding, SymbolType type, ELF elf, ushort sectionIdx)
+        internal SymbolEntry(string name, ulong value, ulong size, SymbolBinding binding, SymbolType type, ELF elf, ushort sectionIdx)
         {
             Name = name;
-            Value = value;
-            Size = size;
+            LongValue = value;
+            LongSize = size;
             Binding = binding;
             Type = type;
             this.elf = elf;
@@ -39,7 +40,7 @@ namespace ELFSharp
         public override string ToString()
         {
             return string.Format("[{3} {4} {0}: 0x{1:X}, size: {2}, section: {5}]",
-                                 Name, Value, Size, Binding, Type, (SpecialSection)sectionIdx);
+                                 Name, LongValue, LongSize, Binding, Type, (SpecialSection)sectionIdx);
         }
 
         private readonly ELF elf;
