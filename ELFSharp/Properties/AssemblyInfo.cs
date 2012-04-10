f2b1c9ba0b06fbf89ed10cb3d17ae775e25159e6
Konrad M. Kruczynski <konrad.kruczynski@gmail.com>
2012-04-10 22:31:59 +0200

    Minor clean-up in AssemblyInfo.

--- ELFSharp/Properties/AssemblyInfo.cs
+++ ELFSharp/Properties/AssemblyInfo.cs
@@ -7,13 +7,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Konrad Kruczyński")]
 [assembly: AssemblyProduct("ELFSharp")]
-[assembly: AssemblyCopyright("Copyright © Konrad Kruczyński 2011")]
+[assembly: AssemblyCopyright("Copyright © Konrad Kruczyński 2011 - 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5070c38b-6c56-4460-8a56-fea4aec5081f")]
 
 [assembly: AssemblyVersion("0.0.3.*")]
