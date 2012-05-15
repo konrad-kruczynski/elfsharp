c0dfe6ec618fe36ad3e92a2ca31166a57602639f
Konrad M. Kruczynski <konrad.kruczynski@gmail.com>
2012-05-15 23:11:25 +0200

    AssemblyInfo cleanup.
    Removed from test, cleaned up in main project.

--- ELFSharp/AssemblyInfo.cs
+++ ELFSharp/AssemblyInfo.cs
@@ -1,16 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ELFSharp")]
 [assembly: AssemblyDescription("C# library for manipulating binary ELF files")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Konrad Kruczyński")]
 [assembly: AssemblyProduct("ELFSharp")]
 [assembly: AssemblyCopyright("Copyright © Konrad Kruczyński 2011 - 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-[assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion("0.0.4.*")]
+[assembly: AssemblyVersion("0.0.5.*")]
