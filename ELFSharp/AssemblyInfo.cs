de079f0ed21093d2aa9ac1d91181e81f4f7b6472
Konrad M. Kruczynski <kkruczynski@antmicro.com>
2013-03-08 17:00:04 +0100

    Bumped version.
    This is needed to be in sync with examples on the webpage.

--- ELFSharp/AssemblyInfo.cs
+++ ELFSharp/AssemblyInfo.cs
@@ -4,6 +4,6 @@
 [assembly: AssemblyDescription("C# library for manipulating binary ELF files")]
 [assembly: AssemblyCompany("Konrad Kruczyński")]
 [assembly: AssemblyProduct("ELFSharp")]
-[assembly: AssemblyCopyright("Copyright © Konrad Kruczyński 2011 - 2012")]
+[assembly: AssemblyCopyright("Copyright © Konrad Kruczyński 2011 - 2013")]
 
-[assembly: AssemblyVersion("0.0.6.*")]
+[assembly: AssemblyVersion("0.0.7.*")]
