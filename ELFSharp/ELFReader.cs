f7f8ad824e03d8f386dff91671f96790b0439107
Konrad M. Kruczynski <kkruczynski@antmicro.com>
2013-08-28 13:10:24 +0200

    Fixes #11.
    Added new API for ELF file testing. Test included.

--- ELFSharp/ELFReader.cs
+++ ELFSharp/ELFReader.cs
@@ -7,36 +7,82 @@ namespace ELFSharp
 	{
 		public static IELF Load(string fileName)
 		{
-			switch(CheckClass(fileName))
+			IELF elf;
+			if(!TryLoad(fileName, out elf))
+			{
+				throw new ArgumentException("Given file is not proper ELF file.");
+			}
+			return elf;
+		}
+
+		public static bool TryLoad(string fileName, out IELF elf)
+		{
+			switch(CheckELFType(fileName))
 			{
 			case Class.Bit32:
-				return new ELF<uint>(fileName);
+				elf = new ELF<uint>(fileName);
+				return true;
 			case Class.Bit64:
-				return new ELF<long>(fileName);
+				elf = new ELF<long>(fileName);
+				return true;
+			default:
+				elf = null;
+				return false;
 			}
-			throw new InvalidOperationException("Should not reach here.");
+		}
+
+		public static Class CheckELFType(string fileName)
+		{
+			var size = new FileInfo(fileName).Length;
+			if(size < Consts.MinimalELFSize)
+			{
+				return Class.NotELF;
+			}
+			using(var reader = new BinaryReader(File.OpenRead(fileName)))
+			{
+				var magic = reader.ReadBytes(4);
+				for(var i = 0; i < 4; i++)
+				{
+					if(magic[i] != Magic[i])
+					{
+						return Class.NotELF;
+					}
+				}
+				var value = reader.ReadByte();
+				return value == 1 ? Class.Bit32 : Class.Bit64;
+			}
 		}
         
 		public static ELF<T> Load<T>(string fileName) where T : struct
 		{
 			return new ELF<T>(fileName);
 		}
-        
-		private static Class CheckClass(string fileName)
+
+		public static bool TryLoad<T>(string fileName, out ELF<T> elf) where T : struct
 		{
-			if(new FileInfo(fileName).Length < 5)
+			bool result;
+			switch(CheckELFType(fileName))
 			{
-				// return any, cause file is corrupted
-				return Class.Bit32;
-			}
-			using(var stream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
-			{
-				stream.Seek(4, SeekOrigin.Begin);
-				var value = stream.ReadByte();
-				// if not 32, then 64 or unknown
-				return value == 1 ? Class.Bit32 : Class.Bit64;
+			case Class.Bit32:
+				result = typeof(T) == typeof(uint);
+				elf = result ? (ELF<T>)(object)new ELF<uint>(fileName) : null;
+				return result;
+			case Class.Bit64:
+				result = typeof(T) == typeof(long);
+				elf = result ? (ELF<T>)(object)new ELF<long>(fileName) : null;
+				return result;
+			default:
+				elf = null;
+				return false;
 			}
 		}
+
+		private static readonly byte[] Magic = new byte[] {
+			0x7F,
+			0x45,
+			0x4C,
+			0x46
+		}; // 0x7F 'E' 'L' 'F'
         
 	}
 }
