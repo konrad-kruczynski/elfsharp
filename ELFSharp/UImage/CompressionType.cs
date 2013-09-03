8ff1e67f48bd997920a349bee627a01f01de76f4
Konrad M. Kruczynski <kkruczynski@antmicro.com>
2013-09-03 14:55:53 +0200

    Image extraction improved.

--- ELFSharp/UImage/CompressionType.cs
+++ ELFSharp/UImage/CompressionType.cs
@@ -2,10 +2,13 @@ using System;
 
 namespace ELFSharp.UImage
 {
-	public enum CompressionType
+	public enum CompressionType : byte
 	{
-		None,
-		Gzip
+		None = 0,
+		Gzip = 1,
+		Bzip2 = 2,
+		Lzma = 3,
+		Lzo = 4
 	}
 }
 
---
+++ ELFSharp/UImage/ImageDataResult.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ELFSharp.UImage
+{
+	public enum ImageDataResult
+	{
+		OK,
+		BadChecksum,
+		UnsupportedCompressionFormat
+	}
+}
+
