2b8b213bcc05fc2b294488f39b67f7f19ad8fbb6
Konrad M. Kruczynski <konrad.kruczynski@gmail.com>
2014-12-05 10:10:32 +0100

    Some tests for PR #23.

---
+++ Tests/ELF/SectionGettingTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using ELFSharp.ELF;
+using ELFSharp.ELF.Sections;
+
+namespace Tests.ELF
+{
+    [TestFixture]
+    public class SectionGettingTests
+    {
+        [Test]
+        public void ShouldGetSection()
+        {
+            var elf = ELFReader.Load(Utilities.GetBinaryLocation("issue3"));
+            elf.GetSection(".rodata");
+        }
+
+        [Test]
+        public void ShouldHandleNonExistingSection()
+        {
+            ISection section;
+            Assert.IsFalse(ELFReader.Load(Utilities.GetBinaryLocation("issue3")).TryGetSection(".nonexisting", out section));
+        }
+
+        [Test]
+        public void ShouldHandleOutOfRangeSection()
+        {
+            ISection section;
+            Assert.IsFalse(ELFReader.Load(Utilities.GetBinaryLocation("issue3")).TryGetSection(28, out section));
+        }
+
+    }
+}
+
