bd20abb200952de82e18f4b375dddc1a0f940038
Konrad M. Kruczyński <konrad.kruczynski@gmail.com>
2015-03-11 22:10:23 +0100

    Version bump.

--- ELFSharp/AssemblyInfo.cs
+++ ELFSharp/AssemblyInfo.cs
@@ -4,6 +4,6 @@
 [assembly: AssemblyDescription("C# library for reading binary ELF files")]
 [assembly: AssemblyCompany("Konrad Kruczyński")]
 [assembly: AssemblyProduct("ELFSharp")]
-[assembly: AssemblyCopyright("Copyright © Konrad Kruczyński 2011 - 2014")]
+[assembly: AssemblyCopyright("Copyright © Konrad Kruczyński 2011 - 2015")]
 
-[assembly: AssemblyVersion("0.0.11")]
+[assembly: AssemblyVersion("0.1")]
