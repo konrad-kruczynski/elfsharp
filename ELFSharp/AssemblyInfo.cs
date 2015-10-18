3756b219954b2fd7c8b7c2062ec6b3eceec183ba
Konrad M. Kruczynski <kkruczynski@antmicro.com>
2015-10-18 16:40:19 +0200

    Version bump.

--- ELFSharp/AssemblyInfo.cs
+++ ELFSharp/AssemblyInfo.cs
@@ -6,4 +6,4 @@
 [assembly: AssemblyProduct("ELFSharp")]
 [assembly: AssemblyCopyright("Copyright © Konrad Kruczyński 2011 - 2015")]
 
-[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyVersion("0.1.1")]
