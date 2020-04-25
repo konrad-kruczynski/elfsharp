4f340272bf1c7ebd81dff97eeef458cbefa817de
Konrad M. Kruczyński <konrad.kruczynski@gmail.com>
2020-04-25 11:12:10 +0200

    Added tests for #66.

--- Tests/MachO/SegmentTests.cs
+++ Tests/MachO/SegmentTests.cs
@@ -26,6 +26,16 @@ namespace Tests.MachO
             var data = segment.Sections.Single(x => x.Name == "__cstring").GetData();
             Assert.AreEqual(Encoding.ASCII.GetBytes("Hello world\0"), data);
         }
+
+        [TestCase("__TEXT", "__text")]
+        [TestCase("__DATA", "__bss")]
+        public static void ShouldFindSectionWithDataAndNoDataArm64(string segmentName, string sectionName)
+        {
+            var machO = MachOReader.Load(Utilities.GetBinaryStream(@"3dengine_libretro_ios.dylib"), true);
+            var segment = machO.GetCommandsOfType<Segment>().Single(x => x.Name == "__TEXT");
+            var section = segment.Sections.Single(x => x.Name == "__text");
+            section.GetData();
+        }
     }
 }
 
