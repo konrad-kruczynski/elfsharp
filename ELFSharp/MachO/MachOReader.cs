c3e4502a7e10397085b936e0322a7f84e8110222
Konrad M. Kruczyński <konrad.kruczynski@gmail.com>
2020-05-31 15:59:46 +0200

    [#68] Handle fat Mach-O binaries.
    Fixes #68.

--- ELFSharp/MachO/MachOReader.cs
+++ ELFSharp/MachO/MachOReader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace ELFSharp.MachO
@@ -17,9 +18,21 @@ namespace ELFSharp.MachO
             return (TryLoad(stream, shouldOwnStream, out MachO result)) switch
             {
                 MachOResult.OK => result,
-                MachOResult.NotMachO => throw new InvalidOperationException("Given file is not a Mach-O file."),
+                MachOResult.NotMachO => throw new InvalidOperationException(NotMachOErrorMessage),
+                MachOResult.FatMachO => throw new InvalidOperationException(FatArchiveErrorMessage),
                 _ => throw new ArgumentOutOfRangeException(),
             };
+        }
+
+        public static IReadOnlyList<MachO> LoadFat(Stream stream, bool shouldOwnStream)
+        {
+            var result = TryLoadFat(stream, shouldOwnStream, out var machOs);
+            if(result == MachOResult.OK || result == MachOResult.FatMachO)
+            {
+                return machOs;
+            }
+
+            throw new InvalidOperationException(NotMachOErrorMessage);
         }
 
         public static MachOResult TryLoad(string fileName, out MachO machO)
@@ -29,16 +42,40 @@ namespace ELFSharp.MachO
 
         public static MachOResult TryLoad(Stream stream, bool shouldOwnStream, out MachO machO)
         {
-            machO = null;
+            var result = TryLoadFat(stream, shouldOwnStream, out var machOs);
+
+            if(result == MachOResult.OK)
+            {
+                machO = machOs.SingleOrDefault();
+            }
+            else
+            {
+                machO = null;
+            }
+
+            return result;
+        }
+
+        public static MachOResult TryLoadFat(Stream stream, bool shouldOwnStream, out IReadOnlyList<MachO> machOs)
+        {
+            machOs = null;
 
             using var reader = new BinaryReader(stream, Encoding.UTF8, true);
             var magic = reader.ReadUInt32();
+
+            if(magic == FatMagic)
+            {
+                machOs = FatArchiveReader.Enumerate(stream, shouldOwnStream).ToArray();
+                return MachOResult.FatMachO;
+            }
+
             if(!MagicToMachOType.TryGetValue(magic, out var machOType))
             {
                 return MachOResult.NotMachO;
             }
 
-            machO = new MachO(stream, machOType.Is64Bit, machOType.Endianess, shouldOwnStream);
+            var machO = new MachO(stream, machOType.Is64Bit, machOType.Endianess, shouldOwnStream);
+            machOs = new[] { machO };
             return MachOResult.OK;
         }
 
@@ -50,6 +87,9 @@ namespace ELFSharp.MachO
             { 0xCFFEEDFE, (false, Endianess.LittleEndian) }
         };
 
+        private const uint FatMagic = 0xBEBAFECA;
+        private const string FatArchiveErrorMessage = "Given file is a fat archive, contains more than one MachO binary. Use (Try)LoadFat to handle it.";
+        private const string NotMachOErrorMessage = "Given file is not a Mach-O file.";
     }
 }
 
