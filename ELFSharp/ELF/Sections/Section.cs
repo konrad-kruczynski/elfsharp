77da2d08dbdea55294ff38aea2e608d9a0c6dde6
Konrad Kruczyński <konrad.kruczynski@gmail.com>
2021-11-10 23:22:01 +0100

    Refactor NoBits case.

--- ELFSharp/ELF/Sections/Section.cs
+++ ELFSharp/ELF/Sections/Section.cs
@@ -15,7 +15,10 @@ namespace ELFSharp.ELF.Sections
         public virtual byte[] GetContents()
         {
             if (Type == SectionType.NoBits)
-                return new byte[] {};
+            {
+                return Array.Empty<byte>();
+            }
+            
             Reader.BaseStream.Seek((long)Header.Offset, SeekOrigin.Begin);
             return Reader.ReadBytes(Convert.ToInt32(Header.Size));
         }
